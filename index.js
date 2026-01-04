284a3763fa3029943a397a8f7603475ebe003159
KAKOLY-AKHTER <kakoly196@gmail.com>
2026-01-04 17:20:20 +0600

    final update project

--- index.js
+++ index.js
@@ -33,25 +33,6 @@ const client = new MongoClient(uri, {
 // Middleware: Verify Firebase Token
 
 
-const verifyAdmin = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) return res.status(401).send({ message: "Unauthorized" });
-
-    const decoded = await admin.auth().verifyIdToken(token);
-    const user = await usersCollection.findOne({ email: decoded.email });
-
-    if (user?.role !== "admin") {
-      return res.status(403).send({ message: "Forbidden: Admin only" });
-    }
-    next();
-  } catch {
-    res.status(401).send({ message: "Unauthorized" });
-  }
-};
-
-
-
 const verifyToken = async (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
